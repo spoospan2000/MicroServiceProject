b8c2c3c6d5b342f9ee354d5eab0a33565cbacd11
Spoorthy I <spoospan2000@gmail.com>
2022-03-29 14:26:54 +0530

    file added

---
+++ Microservice/UserMicroservice/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserMicroservice.Data;
+using UserMicroservice.Data.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserDbContext db;
+        public UserController()
+        {
+            db = new UserDbContext();
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created,model);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,ex);
+            }
+        }
+    }
+}
---
+++ Microservice/UserMicroservice/Data/UserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserMicroservice.Data.Entities;
+
+namespace UserMicroservice.Data
+{
+    public class UserDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = LAPTOP-HDHO2R7F\\MSSQLSERVER01; Database = Microservice; Integrated security = true");
+        }
+    }
+}
